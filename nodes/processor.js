4e9a4e331ab59cde4c51cfa05d5ddaf74c83b8fa
Scott Chapman <scottchapman@us.ibm.com>
2019-01-31 14:21:54 -0500

    handle state

--- nodes/processor.js
+++ nodes/processor.js
@@ -17,8 +17,31 @@
 const validator = require('validator')
 const _ = require('lodash')
 const NodeCache = require( "node-cache" );
-const deviceCache = new NodeCache( { stdTTL: 10*60, checkperiod: 60 } );
-const hostCache = new NodeCache( { stdTTL: 30*60, checkperiod: 60 } );
+const deviceCache = new NodeCache( { stdTTL: 5*60, checkperiod: 60 } );
+const hostCache = new NodeCache( { stdTTL: 5*60, checkperiod: 60 } );
+
+function saveState(node) {
+    node.log("Saving state")
+    var state = {
+        devices: {},
+        hosts: {}
+    };
+    for (var device of deviceCache.keys()) {
+        node.log("Saving state of device " + device)
+        state.devices[device] = deviceCache.get(device);
+    }
+    for (var host of hostCache.keys()) {
+        node.log("Saving state of host " + host)
+        state.hosts[host] = hostCache.get(host);
+    }
+    node.log("Persisting State: " + JSON.stringify(state))
+    node.brokerConn.publish({
+        topic: 'precense-scanner/state',
+        payload: JSON.stringify(state),
+        qos:2,
+        retain: true
+    })
+}
 
 function listen(node) {
     node.brokerConn.register(node)
@@ -97,6 +120,9 @@ function heartbeatListener(node) {
     node.on('close', done => {
         node.brokerConn.unsubscribe(topic,id);
     })
+    setInterval(() => {
+        saveState(node)
+    },1*60*1000)
 }
 
 module.exports = function (RED) {
@@ -106,8 +132,22 @@ module.exports = function (RED) {
         RED.nodes.createNode(this, config);
         this.broker = config.broker;
         this.brokerConn = RED.nodes.getNode(this.broker);
+        var node = this;
         if (this.brokerConn) {
             listen(this)
+            var id = 3;
+            this.brokerConn.subscribe('/st-presence/state',2, (topic,payload,packet) => {
+                var state = JSON.parse(payload.toString());
+                for (var host of _keys(state.hosts)) {
+                    node.log("Restoring state of host: " + host)
+                    hostCache.set(host,state.hosts[host])
+                }
+                for (var device of _keys(state.devices)) {
+                    node.log("Restoring state of device: " + device)
+                    deviceCache.set(device,state.devices[device])
+                }
+                node.unsubscribe('/st-presence/state',2);
+            },id)
         } else {
             this.error(RED._('mqtt.errors.missing-config'));
         }
